7c3bcb9e5a492af2a60f3475e17b345b2a9d6a67
BIThooven <yassine.parkourist@gmail.com>
2023-07-13 10:18:40 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /*
 *main - (A function that prints the output of puts function)
+*Description : using the main function
+*this program prints Programming is like building a multilingual puzzle
 *return : (0) (success)
 */
 int main(void)
