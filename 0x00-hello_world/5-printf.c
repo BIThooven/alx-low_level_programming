a216bbb2afcd0447db799fda8551e7e602a2ebab
BIThooven <yassine.parkourist@gmail.com>
2023-07-13 10:14:09 -0700

    4-puts.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+*main: a program that prints the output of printf function
+*return: 0 
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
