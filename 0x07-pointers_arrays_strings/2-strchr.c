66f094a3c6b100a2cff01ec406f06a84927312c5
BIThooven <yassine.parkourist@gmail.com>
2023-07-31 14:43:32 +0100

    2-strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	char *f = strchr(s, c);
+
+	return (f);
+}
