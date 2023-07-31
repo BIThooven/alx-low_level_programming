aab496ceb882f4ad7bfc85edb470e8bbf44540dd
BIThooven <yassine.parkourist@gmail.com>
2023-07-31 20:55:19 +0100

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - filling the block of memory with a desired value
  * @s: starting address of memory to be filled
- * @b: the desired value to put 
+ * @b: the desired value to put
  * @n: number of bytes to be changed
  *
  * Return: changed array with new value for n bytes
