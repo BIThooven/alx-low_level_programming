0b111cda3132547c665ec6d5a655342a23a62eb2
BIThooven <yassine.parkourist@gmail.com>
2023-07-31 15:31:31 +0100

    4-strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strpbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: Always 0 (Success)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *t = strpbrk(s, accept);
+
+	return (t);
+}
