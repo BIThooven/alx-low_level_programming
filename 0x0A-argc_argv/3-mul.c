5c1fc57933489e758f81101970afd3d42720453c
BIThooven <yassine.parkourist@gmail.com>
2023-08-04 22:41:32 +0100

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that returns the multiple of 2 args
+ * @argc:args to count
+ * @argv: pointer to the array of strings
+ * @mul: multiplication
+ * @i: index to ietterate with
+ * Return: always 0 in case of success
+*/
+int main(int argc, char **argv)
+{
+	int mul = 1;
+	int i;
+
+	if (argc < 1)
+	{
+		perror("error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+	}
+	printf("%d\n", mul);
+	return (0);
+}
