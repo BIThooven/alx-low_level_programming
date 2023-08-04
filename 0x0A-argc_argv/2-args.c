89e1cb1e58d692b9bd690f5474bb2cd28ed56602
BIThooven <yassine.parkourist@gmail.com>
2023-08-04 21:23:25 +0100

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+/**
+* main - a function for the prompt to print what's entered
+*@argc : arguments count
+*@argv : pointer to the array of strings
+*Return: Alwyas 0 indicating success
+*/
 int main(int argc, char **argv)
 {
 	while (argc--)
