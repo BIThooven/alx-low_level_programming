b0280845be155a8752766dc7af4444bc9bfe1be2
BIThooven <yassine.parkourist@gmail.com>
2023-08-07 17:51:31 +0100

    alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	arr = (int **)malloc(height * sizeof(int *));
+	arr = malloc(height * sizeof(int *));
 	if (arr == NULL)
 	{
 		return (NULL);
