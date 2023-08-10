64834f1a8a349fe18fb0e9be25195c844c7b5140
BIThooven <yassine.parkourist@gmail.com>
2023-08-10 15:53:01 +0100

    1-string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,14 +10,10 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *res;
 	char *ptr;
 	size_t length_s1;
 	size_t length_s2;
-	unsigned int i;
 
-	length_s1 = strlen(s1);
-	length_s2 = strlen(s2);
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -26,24 +22,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s2 = "";
 	}
+	length_s1 = strlen(s1);
+	length_s2 = strlen(s2);
 	if (n >= length_s2)
 	{
-		n = strlen(s2);
+		n = length_s2;
 	}
-	ptr = malloc(length_s1 + n - 1);
+	ptr = (char *)malloc(length_s1 + n +1);
 	if (ptr == NULL)
 	{
 		return (NULL);
 	}
-	res = ptr;
-	while (*s1 != '\0')
-	{
-		*res++ = *s1++;
-	}
-	for (i = 0; i < n; i++)
-	{
-		*res++ = *s2++;
-	}
-	*res = '\0';
+	strcpy(ptr, s1);
+	strncat(ptr, s2, n);
 	return (ptr);
 }
