63a121779be33293889efedd793b1814c697f46b
BIThooven <yassine.parkourist@gmail.com>
2023-08-14 12:29:38 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ * Description: A dog traits structure
+*/
 struct dog
 {
 	char *name;
