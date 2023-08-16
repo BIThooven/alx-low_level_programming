2a99fd9d746a09920746fd299f8ec49d78a8d65d
BIThooven <yassine.parkourist@gmail.com>
2023-08-16 17:50:02 +0100

    get_op_func

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,45 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a function that does calculus operations
+ * @argc: number of operations
+ * @argv: characters to be converted to integers
+ * Return: returns 0 when success and 99 or 100 or 98 when error occurs
+*/
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operator;
+	int (*operation)(int, int);
+
+    if (argc != 4)
+    {
+        printf("Error\n");
+        exit(98);
+    }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    operator = argv[2];
+
+    if (num2 == 0 && (*operator == '/' || *operator == '%'))
+    {
+        printf("Error\n");
+        exit(100);
+    }
+
+    operation = get_op_func(operator);
+
+    if (operation == NULL)
+    {
+        printf("Error\n");
+        exit(99);
+    }
+
+    result = operation(num1, num2);
+    printf("%d\n", result);
+
+    return (0);
+}
+
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - a function that calculates the sum
+ * @a: a value to be added
+ * @b: a value to be added
+ * Return: returns the sum of the 2 values
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - a function that calculates the sub
+ * @a: a value to be subtracted
+ * @b: a value to subtract
+ * Return: returns subtraction of 2 values
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - a function that calculates the mul
+ * @a: a value to be multiplied
+ * @b: a value to be multiplied
+ * Return: returns the multiplication of 2 values
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - a function that calculates the division
+ * @a: a value to be divised
+ * @b: the divisor value
+ * Return: returns the divison of 2 values
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a function that calculates the modulo
+ * @a: a value to be moduled
+ * @b: a value to be moduled
+ * Return: returns the modulo of 2 values
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
