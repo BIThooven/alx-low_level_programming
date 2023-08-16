c7f0e77d9590f765810a9f859adab6970fbc67c0
BIThooven <yassine.parkourist@gmail.com>
2023-08-16 17:56:33 +0100

    get_op_func

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,33 +13,33 @@ int main(int argc, char *argv[])
 	char *operator;
 	int (*operation)(int, int);
 
-    if (argc != 4)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    num1 = atoi(argv[1]);
-    num2 = atoi(argv[3]);
-    operator = argv[2];
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-    if (num2 == 0 && (*operator == '/' || *operator == '%'))
-    {
-        printf("Error\n");
-        exit(100);
-    }
+	if (num2 == 0 && (*operator == '/' || *operator == '%'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
-    operation = get_op_func(operator);
+	operation = get_op_func(operator);
 
-    if (operation == NULL)
-    {
-        printf("Error\n");
-        exit(99);
-    }
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
 
-    result = operation(num1, num2);
-    printf("%d\n", result);
+	result = operation(num1, num2);
+	printf("%d\n", result);
 
-    return (0);
+	return (0);
 }
 
