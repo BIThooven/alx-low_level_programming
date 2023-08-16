4b00fef5df552259a0f93db3e0849d7c01ffe5bd
BIThooven <yassine.parkourist@gmail.com>
2023-08-16 13:28:39 +0100

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,18 +2,6 @@
 #include <string.h>
 #include "function_pointers.h"
 /**
- * f - a function to print a name
- * @name: an argument for the function , a pointer to char
- * Return: a void func returns nothing
-*/
-void f(char *name)
-{
-	while (*name != '\0')
-	{
-		name++;
-	}
-}
-/**
  * print_name - a functions that takes a function pointer to print a string
  * @name: an argument for the function, a pointer to char
  * @f: a function pointer
