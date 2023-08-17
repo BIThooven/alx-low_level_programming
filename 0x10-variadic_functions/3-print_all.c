c8a86c30a68f9bb1a36a3c96ee471e3f3bb9fc0b
BIThooven <yassine.parkourist@gmail.com>
2023-08-17 20:10:42 +0100

    print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -30,9 +30,7 @@ void print_all(const char * const format, ...)
 			case 's':
 			{
 				s = va_arg(args, char *);
-				if (s == NULL)
-					printf("(nil)");
-				printf("%s", s);
+				printf("%s", (s == NULL) ? "(nil)" : s);
 				break;
 			}
 			default:
