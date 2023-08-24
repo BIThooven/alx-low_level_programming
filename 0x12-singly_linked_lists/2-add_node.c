9a2246a706a93aa2ab536316122ca5a2ba5facca
BIThooven <yassine.parkourist@gmail.com>
2023-08-24 17:25:20 +0100

    insert node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -5,44 +5,31 @@
  * add_node - a function to insert a new node to a linked list
  * @head: the head node
  * @str: a string to be printed
- * Return: returns the newly created string
+ * Return: returns the newly created string, null if it fails
 */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
+	unsigned int length;
 
+	length = 0;
 	if (!str)
 	{
 		return (NULL);
 	}
-	new_node = (list_t *)malloc(sizeof(list_t));
+	while (str[length])
+	{
+		length++;
+	}
+	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 	{
 		return (NULL);
 	}
 	new_node->str = strdup(str);
-	if (!new_node->str)
-	{
-		free(new_node);
-		return (NULL);
-	}
+	new_node->len = length;
 	new_node->next = *head;
 	*head = new_node;
 
-	return (new_node);
-}
-/**
- * print_lst - a function to print the list
- * @head: the head node
- * Return: void returns nothing
-*/
-void print_lst(list_t *head)
-{
-	list_t *current = head;
-
-	while (current != NULL)
-	{
-		printf("[%ld] %s\n", strlen(current->str), current->str);
-		current = current->next;
-	}
+	return (*head);
 }
