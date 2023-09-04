2bb0917194be315b8fc05bf1aac165beb69ec8b6
BIThooven <yassine.parkourist@gmail.com>
2023-09-04 13:40:02 +0100

    append string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -14,20 +14,22 @@ int append_text_to_file(const char *filename, char *text_content)
 	ssize_t text_len;
 	ssize_t bytes_written;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-	fd = open(filename, O_WRONLY | O_APPEND | O_CREAT, S_IRUSR | S_IWUSR);
+	if (text_content == NULL)
+	{
+		for (text_len = 0; text_content[text_len];)
+			text_len++;
+	}
+	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 	{
 		return (-1);
 	}
-
 	text_len = strlen(text_content);
 	bytes_written = write(fd, text_content, text_len);
-
 	if (bytes_written == -1)
 	{
 		close(fd);
