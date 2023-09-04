1a23eafd3c48df38e912749629340f3e83310f8e
BIThooven <yassine.parkourist@gmail.com>
2023-09-04 13:33:27 +0100

    append string

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t text_len;
+	ssize_t text_len;
 	ssize_t bytes_written;
 
 	if (filename == NULL || text_content == NULL)
