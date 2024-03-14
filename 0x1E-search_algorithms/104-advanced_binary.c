895744b5aaa145177e36cf3f86c94ffb2e2326b7
BIThooven <yassine.parkourist@gmail.com>
2024-03-14 00:53:06 +0000

    advanced_binary search algo

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -9,31 +9,44 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t left;
-	size_t right;
-	size_t mid, i;
-
-	if (array == NULL || value == 0 || size == 0)
+	if (array == NULL || size == 0)
 		return (-1);
+	return (recursive_search(array, 0, size - 1, value));
+}
+/**
+ * recursive_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @left: left index
+ * @right: right index
+ * @value: value to search for
+ * Return: the index where value is located, -1 if value is not present in
+ */
+int recursive_search(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
 
-	for (left = 0, right = size - 1; left <= right;)
+	if (left == right)
 	{
-		printf("Searching in array: ");
-		for (i = left; i < right; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
-		mid = (left + right) / 2;
-		if (array[mid] < value)
-			left = mid + 1;
-		else if (array[mid] > value)
-			right = mid - 1;
-		else
-		{
-			if (array[mid - 1] == value)
-				right = mid;
-			else
-				return (mid);
-		}
+		if (array[left] == value)
+			return (left);
+		return (-1);
 	}
-	return (-1);
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+	i = left + (right - left) / 2;
+	if (array[i] == value)
+	{
+		if (array[i - 1] < value)
+			return (i);
+	}
+	if (array[i] < value)
+		return (recursive_search(array, i + 1, right, value));
+	return (recursive_search(array, left, i, value));
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,4 +10,5 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+int recursive_search(int *array, size_t left, size_t right, int value);
 #endif
