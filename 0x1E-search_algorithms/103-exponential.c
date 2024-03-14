8d7efaa8c9f3e32435a7e4f9049e7e597500300a
BIThooven <yassine.parkourist@gmail.com>
2024-03-14 00:34:34 +0000

    exponential sorting algo

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -23,3 +23,36 @@ int exponential_search(int *array, size_t size, int value)
 	printf("Value found between indexes [%lu] and [%lu]\n", min, max);
 	return (binary_search(array + min, max - min + 1, value) + min);
 }
+/**
+ * binary_search - searches for a value in a sorted array of integers using the
+ * Binary search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ * Return: the index where value is located, -1 if value is not present in
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left;
+	size_t right;
+	size_t mid, i;
+
+	if (array == NULL || value == 0 || size == 0)
+		return (-1);
+
+	for (left = 0, right = size - 1; left <= right;)
+	{
+		printf("Searching in array: ");
+		for (i = left; i < right; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+		mid = (left + right) / 2;
+		if (array[mid] < value)
+			left = mid + 1;
+		else if (array[mid] > value)
+			right = mid - 1;
+		else
+			return (mid);
+	}
+	return (-1);
+}
